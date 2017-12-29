5f595d3ed4d24289bff292f8f39d7b74ef7336aa
appucyriac <appu@qburst.com>
2017-12-29 17:31:29 +0530

    auditee login and dashboard

--- src/components/Auditee.jsx
+++ src/components/Auditee.jsx
@@ -25,40 +25,43 @@ class Auditee extends React.Component {
   }
  render() {
     return (
-      <div className="auditeeBox">
-      <Card className="userImage">
-			<CardMedia style={{
-				backgroundImage: 'url(https://assets1.sharetribe.com/assets/profile_image/medium/missing-c54f2181aa6b87c9c74bbe1ed8876086c314ffcf318441f2f7d70daf736fc3f2.png)',
-				height: '12.313rem',
-				width: '12.5rem'
+      
+      
 
-				
-			}}>
-			</CardMedia>
-			<CardPrimary>
-				<CardTitle large>Auditee</CardTitle>
-				<CardSubtitle>Emp No: 123456</CardSubtitle>
- 			</CardPrimary>
-     </Card>
-        <Typography use="title">List of ratings</Typography>
-		<Grid>
-			<GridCell span="4"><Typography use="subheading2">Rating</Typography>
-				<GridCell span="4">3</GridCell>
-		    </GridCell>
-			<GridCell span="4"><Typography use="subheading2">Duration</Typography>
-			    <GridCell span="4">April 2017 - October 2017</GridCell>
-			</GridCell>
-			<GridCell span="4"><Typography use="subheading2">Comments</Typography>
-			    <GridCell span="4">Half yearly</GridCell>
-			</GridCell>
-			<Snackbar
-		show={this.state.snackbarIsOpen}
-	onClose={evt => this.setState({snackbarIsOpen: false})}
-	message="Welcome"
-	actionText="Dismiss"
-	
-/>
-		</Grid>
+      <div className="auditeeBox">
+      <Typography use="headline" className="title">Auditee Dashboard</Typography>
+	      <Card className="userImage">
+				<CardMedia style={{
+					backgroundImage: 'url(https://assets1.sharetribe.com/assets/profile_image/medium/missing-c54f2181aa6b87c9c74bbe1ed8876086c314ffcf318441f2f7d70daf736fc3f2.png)',
+					height: '12.313rem',
+					width: '12.5rem'
+
+					
+				}}>
+				</CardMedia>
+				<CardPrimary>
+					<CardTitle large>Auditee</CardTitle>
+					<CardSubtitle>Emp No: 123456</CardSubtitle>
+	 			</CardPrimary>
+	     </Card>
+	        <Typography use="title">List of ratings</Typography>
+			<Grid>
+				<GridCell span="4"><Typography use="subheading2">Rating</Typography>
+					<GridCell span="4">3</GridCell>
+			    </GridCell>
+				<GridCell span="4"><Typography use="subheading2">Duration</Typography>
+				    <GridCell span="4">April 2017 - October 2017</GridCell>
+				</GridCell>
+				<GridCell span="4"><Typography use="subheading2">Comments</Typography>
+				    <GridCell span="4">Half yearly</GridCell>
+				</GridCell>
+				<Snackbar
+					show={this.state.snackbarIsOpen}
+					onClose={evt => this.setState({snackbarIsOpen: false})}
+					message="Welcome"
+					actionText="Dismiss"
+		        />
+			</Grid>
       </div>
     );
   }
